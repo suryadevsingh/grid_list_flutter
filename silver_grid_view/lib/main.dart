0a7d9d2c2b059af4ecfdf15ecd5d603762a01b2b
Surya Dev Singh <46717624+suryadevsingh@users.noreply.github.com>
2019-07-29 23:42:08 +0530

    Add files via upload

---
+++ silver_grid_view/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Grid List';
+
+    return MaterialApp(
+      theme: new ThemeData(
+        brightness: Brightness.dark,                        // new
+      ),
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: List.generate(100, (index) {
+            return Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline,
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
